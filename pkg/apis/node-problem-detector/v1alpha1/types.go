e2df4bcc05a63b3edeebca03ac5b06ecc882f9fb
Joel Smith <joesmith@redhat.com>
2018-08-08 10:08:13 -0600

    INITIAL COMMIT

---
+++ pkg/apis/node-problem-detector/v1alpha1/types.go
@@ -0,0 +1,29 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type NodeProblemDetectorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []NodeProblemDetector `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type NodeProblemDetector struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              NodeProblemDetectorSpec   `json:"spec"`
+	Status            NodeProblemDetectorStatus `json:"status,omitempty"`
+}
+
+type NodeProblemDetectorSpec struct {
+	// Fill me
+}
+type NodeProblemDetectorStatus struct {
+	// Fill me
+}
